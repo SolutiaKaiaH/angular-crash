23974ff50daa4df488e7aa146da52e792eef6aee
SolutiaKaiaH <kaia_hansen@solutiaconsulting.com>
2023-02-20 11:34:23 -0600

    Add Task

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -31,4 +31,9 @@ export class TasksComponent {
     task.reminder = !task.reminder;
     this.taskService.updateTaskReminder(task).subscribe();
   }
+
+  addTask(task: Task) {
+    //add to backend
+    this.taskService.addTask(task).subscribe((task) => this.tasks.push(task));
+  }
 }
--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -32,4 +32,8 @@ export class TaskService {
     //put request because making update
     return this.http.put<Task>(url, task, httpOptions);
   }
+
+  addTask(task: Task): Observable<Task> {
+    return this.http.post<Task>(this.apiUrl, task, httpOptions);
+  }
 }
